60ba87b9fd9277940a64da34eeaf79f28121e52d
atonizer <atonizer@gmail.com>
2016-08-26 04:55:07 -0400

    Webapp java project

--- src/edu/javalearn/app/model/Contact.java
+++ src/edu/javalearn/app/model/Contact.java
@@ -6,15 +6,15 @@ package edu.javalearn.app.model;
  * Webapp
  */
 public class Contact {
-    private final String type;
+    private final ContactType type;
     private final String value;
 
-    public Contact(String type, String value) {
+    public Contact(ContactType type, String value) {
         this.type = type;
         this.value = value;
     }
 
-    public String getType() {
+    public ContactType getType() {
         return type;
     }
 
---
+++ src/edu/javalearn/app/model/ContactType.java
@@ -0,0 +1,15 @@
+package edu.javalearn.app.model;
+
+/**
+ * Created by user
+ * on 8/26/16.
+ * Webapp
+ */
+public enum ContactType {
+    PHONE,
+    MOBILE,
+    HOME_PHONE,
+    SKYPE,
+    MAIL,
+    QQ
+}
